a1ffc0d79b76671f3a95de792948c6642ad18965
Navaneeth Sai Patti <navaneethsaipatti@gmail.com>
2023-07-31 18:37:53 +0530

    Basic setup was done

---
+++ src/utils/common/enums.js
@@ -0,0 +1,24 @@
+const SEAT_TYPE = {
+    REGULAR : "regular",
+    PREMIUM : "premium",
+    VVIP : "vvip"
+}
+
+const SHOW_STATUS = {
+    UPCOMING : "upcoming",
+    RUNNING : "running",
+    FINISHED : "finished",
+    CANCELLED : "cancelled"
+}
+
+const SEAT_STATUS = {
+    AVAILABLE : "available",
+    BOOKED : "booked",
+    LOCKED : "locked"
+}
+
+module.exports = {
+    SEAT_TYPE,
+    SHOW_STATUS,
+    SEAT_STATUS
+}
